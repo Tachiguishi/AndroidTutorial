ba54c1f039c2d9d986032aa064077283406abc07
TachiTukimi <TachiguishiRor@gmail.com>
2017-01-10 22:50:40 +0800

    Make false_button toasts

--- GeoQuiz/app/src/main/java/com/avalon/ash/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/avalon/ash/geoquiz/QuizActivity.java
@@ -28,7 +28,9 @@ public class QuizActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public  void onClick(View v){
-                // Do nothing yet, but soon.
+                Toast.makeText(QuizActivity.this,
+                        R.string.incorrect_toast,
+                        Toast.LENGTH_SHORT).show();
             }
         });
     }
