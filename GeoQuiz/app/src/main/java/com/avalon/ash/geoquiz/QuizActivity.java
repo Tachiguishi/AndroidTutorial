bffbb6a64cb14bd5ae51d7d8463bfe6d585e61e5
TachiTukimi <TachiguishiRor@gmail.com>
2017-02-26 17:19:31 +0800

    fix bug: 旋转后问题变成第一个问题

--- GeoQuiz/app/src/main/java/com/avalon/ash/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/avalon/ash/geoquiz/QuizActivity.java
@@ -10,6 +10,7 @@ import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
     private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -49,6 +50,10 @@ public class QuizActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
 
+        if(savedInstanceState != null){
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+
         mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
         updateQuestion();
 
@@ -75,6 +80,13 @@ public class QuizActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
     }
 
     @Override
